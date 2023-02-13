ed2dee2eec233dd38259fdfb35f0f7378b61d94c
Raja Soundaramourty <rajasoun@icloud.com>
2023-02-13 11:38:17 -0500

    calculate pod startup time

--- scripts/lib/loader.sh
+++ scripts/lib/loader.sh
@@ -3,6 +3,7 @@
 GIT_BASE_PATH=$(git rev-parse --show-toplevel)
 SCRIPT_LIB_DIR="${GIT_BASE_PATH}/scripts/lib"
 
+source "${SCRIPT_LIB_DIR}/os.sh"
 source "${SCRIPT_LIB_DIR}/checks.sh"
 source "${SCRIPT_LIB_DIR}/env.sh"
 source "${SCRIPT_LIB_DIR}/exception.sh"
@@ -12,3 +13,6 @@ source "${SCRIPT_LIB_DIR}/log.sh"
 source "${SCRIPT_LIB_DIR}/usage.sh"
 source "${SCRIPT_LIB_DIR}/yaml.sh"
 source "${SCRIPT_LIB_DIR}/k8s.sh"
+source "${SCRIPT_LIB_DIR}/pod.sh"
+source "${SCRIPT_LIB_DIR}/table.sh"
+source "${SCRIPT_LIB_DIR}/time.sh"
---
+++ scripts/lib/os.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Pretty Print
+function pretty_print() {
+  printf "%b" "$1"
+}
