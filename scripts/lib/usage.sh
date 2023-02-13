53d91d1efd7a6fde843e665bdf5d3008ddb983cb
Raja Soundaramourty <rajasoun@icloud.com>
2023-02-13 08:56:27 -0500

    end to end setup script

--- scripts/lib/usage.sh
+++ scripts/lib/usage.sh
@@ -4,9 +4,10 @@
 # Description:
 #   Prints usage instructions and a list of valid options to the terminal.
 function usage() {
-    error "Usage: $0 < env | helm | file >"
-    warn "\tAvailable options:"
-    info "\tprecheck:   Runs Pre Checks"
-    info "\t     env:   Exports the cluster environment variables to a file named \"env_vars.csv\" in the current directory"
-    info "\t    helm:   Exports the Helm charts to a directory named \".resources/helm\" in the current directory"
+    error "Usage: $0 < configure | precheck | env | helm  >"
+    warn "\tAvailable options"
+    info "\tconfigure:   Configures the Kubeconfig for the cluster"
+    info "\t precheck:   Runs Pre Checks"
+    info "\t      env:   Exports the cluster environment variables to a file named \"env_vars.csv\" in the current directory"
+    info "\t     helm:   Exports the Helm charts to a directory named \".resources/helm\" in the current directory"
 }
